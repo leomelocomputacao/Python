7cbebaa38d26efa061c9047d7b1845455c7a174a
Leonardo Melo <leomelocomputacao@gmail.com>
2014-06-28 02:40:43 -0300

    Adição Modulo A parte 2

--- Apostilas Grupo Python/Modulo_A-Parte_1/Modulo_A-Parte_1/Modulo_A_Parte_1.py
+++ Apostilas Grupo Python/Modulo_A-Parte_1/Modulo_A-Parte_1/Modulo_A_Parte_1.py
@@ -1,78 +1,78 @@
-print('a = 1') # Comentário em PYTHON - Caluladora
+print 'a = 1' # Comentário em PYTHON - Caluladora
 a = 1
-print(type(a))
+print type(a)
 
-print('\nb = 2')
+print '\nb = 2'
 b = 2
-print(type(b))
+print type(b)
 
-print('\na == b')
-print(a == b)
-print(type(a == b))
+print '\na == b'
+print a == b
+print type(a == b)
 
-print('\na != b')
-print(a != b)
-print(type(a != b))
+print'\na != b'
+print a != b
+print type(a != b)
 
-print('\na <> b')
-print(a <> b)
-print(type(a <> b))
+print '\na <> b'
+print a <> b
+print type(a <> b)
 
-print('\na > b')
-print(a > b)
-print(type(a > b))
+print '\na > b'
+print a > b
+print type(a > b)
 
-print('\na < b')
-print(a < b)
-print(type(a < b))
+print '\na < b'
+print a < b
+print type(a < b)
 
-print('\n2 * a >= b')
-print(2 * a >= b)
-print(type(2 * a >= b))
+print '\n2 * a >= b'
+print 2 * a >= b
+print type(2 * a >= b)
 
-print('\n2 ** 3 => Dois elevado ao cubo')
-print(2 ** 3)
-print(type(2 ** 3))
+print '\n2 ** 3 => Dois elevado ao cubo'
+print 2 ** 3
+print type(2 ** 3)
 
-print('\n2 ** (3 + 6) => Dois elevado a 9')
-print(2 ** (3 + 6))
-print(type(2 ** (3 + 6)))
+print '\n2 ** (3 + 6) => Dois elevado a 9'
+print 2 ** (3 + 6)
+print type(2 ** (3 + 6))
 
-print('\n7 % 2 => 7 modulo 2 (resto da divisão entre 7 e 2)')
-print(7 % 2)
-print(type(7 % 2))
+print '\n7 % 2 => 7 modulo 2 (resto da divisão entre 7 e 2)'
+print 7 % 2
+print type(7 % 2)
 
-print('\na = 1')
+print '\na = 1'
 a = 'Copa do mundo 2014 - Brasil'
-print(a)
-print(type(a))
+print a
+print type(a)
 
-print('\nb = int(\'1\')')
+print '\nb = int(\'1\')'
 b = int('1')
-print(b)
-print(type(b))
+print b
+print type(b)
 
-print('\nc = int(1.2)')
+print '\nc = int(1.2)'
 c = int(1.2)
-print(c)
-print(type(c))
+print c
+print type(c)
 
-print('\nd = float(1)')
+print '\nd = float(1)'
 d = float(1)
-print(d)
-print(type(d))
+print d
+print type(d)
 
-print('\ne = float(\'1.2\')')
+print '\ne = float(\'1.2\')'
 e = float('1.2')
-print(e)
-print(type(e))
+print e
+print type(e)
 
-print('\nf = str(1.2)')
+print '\nf = str(1.2)'
 f = str(1.2)
-print(f)
-print(type(f))
+print f
+print type(f)
 
-print('\ng = str(1)')
+print '\ng = str(1)'
 g = str(1)
-print(g)
-print(type(g))
+print g
+print type(g)
